14e52a6d5d222b0eab494b4c02942d84c8d2b787
Wendy Fgigueroa <camomila444@gmail>
2020-12-07 15:02:50 -0500

    wrk on timeday

---
+++ Assets/script/script.js
@@ -0,0 +1,21 @@
+//* variables///
+var input_texarea = document.querySelector("#input9am");
+var output_div = document.querySelector("#input9am");
+var saveBtn = document.querySelector("#button9am");
+
+
+
+
+
+
+// Open Day Planner and Day and Time is Display at top
+
+var currentDay = DateTime.local()
+$("#currentDay").append(currentDay)
+
+var currentTime = DateTime.local()
+$("#currentTime").append(currentTime)
+
+
+
+
--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 https://getbootstrap.com/docs/4.5/components/input-group/
 
 https://www.codesprogram.com/icons/Font-awesome-save-icon
+
+https://www.w3schools.com/jquery/html_append.asp
+
+https://www.w3schools.com/js/js_output.asp
