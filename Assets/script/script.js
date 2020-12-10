28f11743f1f06a0315f53365bb08e12f326fec25
Wendy Fgigueroa <camomila444@gmail>
2020-12-09 20:30:14 -0500

    wrk on storage

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,11 +1,3 @@
-// //* variables///
-//  var input_textarea = document.querySelector("#input9am");
-//  var output_div = document.querySelector("#input9am");
-//  var saveBtn = document.querySelector("#button9am");
-
-
-
-
 
 // Open Day Planner and Day and Time is Display at top
 
@@ -42,48 +34,60 @@ $.each(timeblock , function (hour) {
 });
 
 
+// function of Time block -click block.
+// Enter event -click save button for that insert time block.
+// Event  is sav e on local storage.
+
+
 $(".saveBtn").on("click" , function(){
     var hourclick = $(this).attr("id")
     console.log($(this) [0].parentElement.previousElementSibling.value)
 })
 
 
+        $ (document).ready(function () {
+          
+          })
+      
+     $("9am").children("innertext").val(localStorage.getItem("9am"));
+     $("10am").children("innertext").val(localStorage.getItem("10am"));
+     $("11am").children("innertext").val(localStorage.getItem("11am"));
+     $("12pm").children("innertext").val(localStorage.getItem("12pm"));
+     $("1pm").children("innertext").val(localStorage.getItem("1pm"));
+     $("2pm").children("innertext").val(localStorage.getItem("2pm"));
+     $("3pm").children("innertext").val(localStorage.getItem("3pm"));
+     $("4pm").children("innertext").val(localStorage.getItem("4pm"));
+     $("5pm").children("innertext").val(localStorage.getItem("5pm"));
 
-
-// function of Time block -click block.
-// Enter event -click save button for that insert time block.
-// Event  is sav e on local storage.
-
-        function hour() { 
-           var input_textarea = document.querySelector("#input9am");
-           var output_div = document.querySelector("#input9am");
-           var saveBtn = document.querySelector("#button9am");
-
-            saveBtn.addEventListener("click" , updateOutput);
      
-            output_div.textContent = localStorage.getItem("content");
-            input_textarea.value = localStorage.getItem("content");
 
-            function updateOutput() { 
-               localStorage.setItem("content" , input_extareaa.value);
+        // function nineAM() { 
+        //     var input_textarea = document.querySelector("#input9am");
+        //     console.log(nineAM)
+        //     var output_div = document.querySelector("#input9am");
+           
+        //     output_div.textContent = localStorage.getItem("content");
+        //     input_textarea.value = localStorage.getItem("content");
 
-                output_div.textContent = input_textarea.value;
-            }
-        }
+        //     function displaytext() { 
+        //       localStorage.setItem("content" , input_extareaa.value);
+        //         output_div.textContent = input_textarea.value;
+        //     }
+        // }
 
-        function hour() { 
-            var input_textarea2 = document.querySelector("#input10am");
-            var output_div2 = document.querySelector("#input10am");
-            var saveBtn2 = document.querySelector("#button10am");
+        // function tenAM() { 
+        //     var input_textarea2 = document.querySelector("#input10am");
+           
+        //     var output_div2 = document.querySelector("#input10am");
+           
+        //     output_div2.textContent = localStorage.getItem("content2");
+        //     input_textarea2.value = localStorage.getItem("content2");
 
-            saveBtn2.addEventListener("click", updateOutput2);
-     
-            output_div2.textContent = localStorage.getItem("content2");
-            input_textarea2.value = localStorage.getItem("content2");
+        //     function updateOutput2() { 
+        //          localStorage.setItem("content2", input_textarea2.value);
+        //          output_div2.textContent = input_textarea2.value;
+        //     }
+        // }
 
-            function updateOutput2() { 
-                localStorage.setItem("content2", input_textarea2.value);
 
-                output_div2.textContent = input_textarea2.value;
-            }
-        }
+
--- README.md
+++ README.md
@@ -16,5 +16,8 @@ https://www.w3schools.com/jquery/html_append.asp
 
 https://www.w3schools.com/js/js_output.asp
 
+https://stackoverflow.com/questions/15466731/jquery-what-is-the-difference-between-p-and-p
+
 LOCAL STORAGEHOW TO USE IN JAVASCRIPT
 https://www.youtube.com/watch?v=k8yJCeuP6I8
+
