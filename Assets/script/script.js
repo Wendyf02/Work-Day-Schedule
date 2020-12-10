bd9bec4f1a85cdd12683c82f77f10305437789c6
Wendy Fgigueroa <camomila444@gmail>
2020-12-09 22:00:07 -0500

    wrk update

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,53 +1,55 @@
 
-// Open Day Planner and Day and Time is Display at top
+ $ (document).ready(function () {
 
-let currentDay = document.getElementById('currentDay');
-let DateTime = luxon.DateTime;
-let today = DateTime.local()
-let f = { month: 'long' , day :'2-digit'};
-let m = today.get('month');
-let tz = today.zoneName;
-currentDay.textContent = tz;
-let newDt = today.set({month: 12});
-currentDay.textContent = today.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
-console.log(today.toISO());
+     // Open Day Planner and Day and Time is Display at top
+
+    let currentDay = document.getElementById('currentDay');
+    let DateTime = luxon.DateTime;
+    let today = DateTime.local()
+    let f = { month: 'long' , day :'2-digit'};
+    let m = today.get('month');
+    let tz = today.zoneName;
+    currentDay.textContent = tz;
+    let newDt = today.set({month: 12});
+    currentDay.textContent = today.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+    console.log(today.toISO());
 
 
 
-// function each hour is colorblock /time block is color coded indicating 
-//  -Past(Gray) Present (Red)  -Future(Green)
+   // function each hour is colorblock /time block is color coded indicating 
+   //  -Past(Gray) Present (Red)  -Future(Green)
 
-var timeblock = $(".hour");
-var now = DateTime.local().c.hour;
+   var timeblock = $(".hour");
+   var now = DateTime.local().c.hour;
 
-$.each(timeblock , function (hour) {
-  var hourId = parseInt($(this).attr("id"));
+    $.each(timeblock , function (hour) {
+    var hourId = parseInt($(this).attr("id"));
     console.log(now,hourId)
-  if (hourId === now) {
-      $(this).addClass("present");
-  } else if(hourId < now) {
-    $(this).addClass("past"); 
-} else if(hourId > now) {
+    if (hourId === now) {
+       $(this).addClass("present");
+    } else if(hourId < now) {
+       $(this).addClass("past"); 
+    } else if(hourId > now) {
     $(this).addClass("future");
-}
+  }
 
-});
-
-
-// function of Time block -click block.
-// Enter event -click save button for that insert time block.
-// Event  is sav e on local storage.
+  });
 
 
-$(".saveBtn").on("click" , function(){
-    var hourclick = $(this).attr("id")
-    console.log($(this) [0].parentElement.previousElementSibling.value)
-})
+   // function of Time block -click block.
+   // Enter event -click save button for that insert time block.
+   // Event  is sav e on local storage.
 
 
-    $ (document).ready(function () {
+  $(".saveBtn").on("click" , function(){
+      var hourclick = $(this).attr("id").split("button")[1];
+      var userInput = ($(this)[0].parentElement.previousElementSibling.value);
+      localStorage.setItem(hourclick, userInput);
+  })
+ 
           
-          
+
+
       $("#9am").val(localStorage.getItem("9am"));
       $("#10am").val(localStorage.getItem("10am"));
       $("#11am").val(localStorage.getItem("11am"));
@@ -60,38 +62,12 @@ $(".saveBtn").on("click" , function(){
 
     
 
-    })
+})
 
 
 
-    $("#9am").val("coding is so fun");
-        // function nineAM() { 
-        //     var input_textarea = document.querySelector("#input9am");
-        //     console.log(nineAM)
-        //     var output_div = document.querySelector("#input9am");
-           
-        //     output_div.textContent = localStorage.getItem("content");
-        //     input_textarea.value = localStorage.getItem("content");
-
-        //     function displaytext() { 
-        //       localStorage.setItem("content" , input_extareaa.value);
-        //         output_div.textContent = input_textarea.value;
-        //     }
-        // }
-
-        // function tenAM() { 
-        //     var input_textarea2 = document.querySelector("#input10am");
-           
-        //     var output_div2 = document.querySelector("#input10am");
-           
-        //     output_div2.textContent = localStorage.getItem("content2");
-        //     input_textarea2.value = localStorage.getItem("content2");
-
-        //     function updateOutput2() { 
-        //          localStorage.setItem("content2", input_textarea2.value);
-        //          output_div2.textContent = input_textarea2.value;
-        //     }
-        // }
+  
+        
 
 
 
